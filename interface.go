a627ab10eb987a0d8456eb0da18262aaeef494f0
souma tsuda <soumatsuda@soumanoMacBook-Pro.local>
2022-07-01 13:35:13 +0900

    Add:

---
+++ interface.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Iuser interface{
+	Create()
+	//・・・メソッドシグニチャ郡を作る
+}
+
+type User struct {
+	Id, Age int
+	Name, Address string
+}
+
+func (u User) Create() {
+	fmt.Println(u.Id)
+}
+
+func main(){
+	var u Iuser = User{
+		Id : 1,
+		Name : "tarou",
+		Age : 20,
+		Address : "Kobe City",
+	}
+	
+	fmt.Printf("uこれ何型??%T\n", u)
+	u.Create()
+}
